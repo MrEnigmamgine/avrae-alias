109ba829b717d3c1ccbcb317950ce23799b1afe8
MrEnigmamgine <mayes.joshua.d@gmail.com>
2021-07-19 14:27:18 -0400

    reorganizing

--- Completed/Transaction/transaction.py
+++ Completed/Transaction/transaction.py
@@ -1,4 +1,3 @@
-  
 !alias transaction embed
 <drac2>
 args=&ARGS&
@@ -6,43 +5,45 @@ coinTypes=["cp","sp","ep","gp","pp"]
 coinRates={"cp":100,"sp":10,"ep":2,"gp":1,"pp":0.1}
 defaultCoin="gp"
 coinPouchName="Coin Pouch"
-bagDict = {i[0]:i[1] for i in load_json(bags)} #load bags as a dictionary
-pouch = bagDict[coinPouchName]
-prevVal = 0
+bagDict={i[0]:i[1] for i in load_json(bags)} #load bags as a dictionary
+pouch=bagDict[coinPouchName]
+prevVal=0
 for coin in coinTypes:
     prevVal += (pouch[coin]/coinRates[coin])
-transactionText = ''
-error = False
-errorText = "¯\_(ツ)_/¯"
-descstr = ''
+transactionText=''
+error=False
+errorText=''
+descstr=''
+change=False
 for i, arg in enumerate(args):
     if not arg.isnumeric():
-        numbers = arg[:-2]
-        letters = arg[(len(arg)-2):].lower()
+        numbers=arg[:-2]
+        letters=arg[(len(arg)-2):].lower()
 
         if (numbers.replace('.','',1).strip('-+').isnumeric()) and (letters in coinTypes):
-            amount = int(numbers)
-            coinType = letters
+            change=True
+            amount=int(numbers)
+            coinType=letters
             pouch[coinType] += amount
             transactionText += arg.strip('+') +', '
         elif (numbers.replace('.','',1).strip('-+').isnumeric()) and (not letters in coinTypes):
             error = True
-            errorText = 'Invalid Coin Type'
+            errorText='Invalid Coin Type'
             break
         elif not letters in coinTypes:
             descstr += arg+' '
         else:
-            error = True
+            error=True
             break
 newVal = 0
 for coin in coinTypes:
     newVal += (pouch[coin]/coinRates[coin])
-if newVal-prevVal == 0:
-    error = True 
-    errorText = "Coins are wrong."
+if (not change) and (newVal-prevVal == 0):
+    error=True 
+    errorText="Coins are wrong."
 elif descstr == '':
-    error = True
-    errorText = "Missing description."
+    error=True
+    errorText="Missing description."
 if not error:
     character().set_cvar("bags", dump_json(list(bagDict.items()) )) #Commit the changes to cvar
     plus = newVal-prevVal
@@ -50,7 +51,7 @@ if not error:
     tText = name+' makes a transaction!'
 else:
     tText = name+' tries to make a transaction!'
-    logText = "Something went wrong." +'\n **Error Message:** '+errorText
+    logText = "Something went wrong.\n**Error Message:** "+errorText
 
 </drac2>
 -title '{{tText}}'
@@ -59,9 +60,6 @@ else:
 -footer '!transaction [+/-]Xgp [+/-]Xsp "Description of Transaction"'
 
 
-################################################################################################
-############### Here begins notes and testing stuff.  Not part of Alias ########################
-################################################################################################
 
 !tra +25gp -10sp 12cp "Description of transaction"
 
